a55578242bba683e1e19703ce345179d440f8a22
Nícolas Silva <96575546+rrashy@users.noreply.github.com>
2022-04-04 15:35:22 -0300

    initial commit

---
+++ terceira-aula/index.php
@@ -0,0 +1,18 @@
+<?php
+    // variaveis
+    $nome = "Helena";
+    $idade = 18;
+    $cidade = "Porto Alegre";
+
+    // Apostrofo sem concatenação imprime exatamente o que está escrito dentro
+
+    echo 'Nome: $nome';
+
+    // Aspas faz a busca da variavel
+    echo "<br> Nome: $nome";
+    echo "<br> Idade: $idade";
+
+    // Apostrofo identifica como texto assim tendo de fazer uma concatenação de forma especifica
+
+    echo '<br> Cidade: '.$cidade
+?>
